4d1453b263db47be4fa4e71616d10371f92af3ea
mishap45 <pavlovmisha45@gmail.com>
2021-03-08 22:36:09 +0200

    Added BottomModal in RaceScreen

--- src/components/RaceButton.tsx
+++ src/components/RaceButton.tsx
@@ -8,15 +8,17 @@ type RaceButtonPropsTypes = {
     text: string
     color: string
     icon: 'marker' | 'unlock'
+    onPress: () => void
 }
 
-const RaceButton:React.FC<RaceButtonPropsTypes> = ({ text, color, icon }) => {
+const RaceButton:React.FC<RaceButtonPropsTypes> = ({ text, color, icon, onPress }) => {
     return <Button
         icon={
             icon === 'marker'
                 ? <MaterialCommunityIcons name="map-marker" size={24} color="#fff" style={styles.icon} />
                 : <FontAwesome5 name="unlock" size={24} color="#fff" style={styles.icon} />
         }
+        onPress={() => onPress()}
         title={text}
         buttonStyle={[{ backgroundColor: color }, styles.block]}
     />
--- src/screens/RaceScreen.tsx
+++ src/screens/RaceScreen.tsx
@@ -1,16 +1,58 @@
-import React from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import React, { useRef } from 'react'
+import { View, Text, StyleSheet, Dimensions, Button } from 'react-native'
+import { NavigationScreenProp } from 'react-navigation'
+import RBSheet from 'react-native-raw-bottom-sheet'
 
 import Header from '../components/Header'
 import RaceButton from '../components/RaceButton'
 
-const RaceScreen = () => {
+type RaceScreenPropsTypes = {
+    navigation: NavigationScreenProp<any, any>
+}
+
+const RaceScreen:React.FC<RaceScreenPropsTypes> = ({ navigation }) => {
+    const refRBSheet = useRef();
+
     return <View>
         <Header text={'Поїздка'}/>
         <View style={styles.container}>
             <Text style={styles.headerTextRace}>У вас немає відкритих поїздок</Text>
-            <RaceButton text={'Найти велосипед'} color={'#323232'} icon={'marker'}/>
-            <RaceButton text={'Розблокувати велосипед'} color={'#ff1e56'} icon={'unlock'}/>
+            <RaceButton onPress={() => navigation.navigate('Map')}
+                        text={'Найти велосипед'} color={'#323232'} icon={'marker'}/>
+            {/*@ts-ignore*/}
+            <RaceButton onPress={() => refRBSheet.current.open()} text={'Розблокувати велосипед'} color={'#ff1e56'} icon={'unlock'}/>
+
+            <View>
+                <RBSheet
+                    ref={refRBSheet}
+                    closeOnDragDown={true}
+                    closeOnPressMask={false}
+                    customStyles={{
+                        wrapper: {
+                            backgroundColor: "transparent",
+                            marginHorizontal: 15,
+                            marginBottom: 60
+                        },
+                        container: {
+                            borderRadius: 25
+                        },
+                        draggableIcon: {
+                            backgroundColor: "#000"
+                        }
+                    }}
+                >
+                    <Text style={styles.bottomText}>Підключайтесь до вашого існуючого акаунту
+                        або створіть новий</Text>
+
+                    <View style={{ marginTop: 30 }}>
+                        <Button title={'Підключитись'} onPress={() => alert('connect')} />
+                    </View>
+
+                    <View style={{ marginTop: 30 }}>
+                        <Button title={'Створити'} onPress={() => alert('create')} />
+                    </View>
+                </RBSheet>
+            </View>
         </View>
     </View>
 };
@@ -27,6 +69,11 @@ const styles = StyleSheet.create({
         fontSize: 32,
         textAlign: 'center',
         paddingBottom: 45
+    },
+    bottomText: {
+        textAlign: 'center',
+        paddingVertical: 10,
+        color: 'rgba(51, 51, 51, .5)'
     }
 });
 
